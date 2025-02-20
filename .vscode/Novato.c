80d3df237ee34a7e6eae5e146f885cd64494a284
Wictor834 <wictorcesar16@gmail.com>
2025-02-20 15:46:54 +0000

    Atualização do nível novato

--- .vscode/Novato.c
+++ .vscode/Novato.c
@@ -2,17 +2,13 @@
 
 int main(){
     
-    char pais[20];
-    char estado[20];
+    char estado [15];
     char cidade[20];
-    int carta;
-    float populacao;
-    int area;
+    char carta[10];
+    int populacao;
+    float area;
     float PIB;
     int turistico;
-
-    printf("Digite o nome do pais: \n");
-    scanf("%s", &pais);
 
     printf("Digite o estado: \n");
     scanf("%s", &estado);
@@ -21,13 +17,13 @@ int main(){
     scanf("%s", &cidade);
 
     printf("Digite o número da carta: \n");
-    scanf("%d", &carta);
+    scanf("%s", &carta);
 
     printf("Digite o número da populacao: \n");
-    scanf("%f", &populacao);
+    scanf("%d", &populacao);
 
     printf("Digite a area: \n");
-    scanf("%d", &area);
+    scanf("%f", &area);
 
     printf("Digite o PIB: \n");
     scanf("%f" , &PIB);
@@ -35,26 +31,24 @@ int main(){
     printf("Digite o número de pontos turisticos: \n");
     scanf("%d" , &turistico);
 
-    printf("O nome do país é: %s \n", pais);
-    printf("O nome do estado é: %s\n" , estado);
+    printf("O nome do estado é: %s \n" , estado);
     printf("O nome da cidade é: %s \n" , cidade);
-    printf("O número da carta é: %d \n", carta);
-    printf("O número da população é: %1.f mil \n", populacao);
-    printf("O tamanho da área em km² é: %d km² \n" , area);
-    printf("O número do PIB é: %1.f bilhões \n", PIB);
+    printf("O número da carta é: %s \n", carta);
+    printf("O número da população é: %d mil \n", populacao);
+    printf("O tamanho da área em km² é: %f km² \n" , area);
+    printf("O número do PIB é: %f bilhões \n", PIB);
     printf("O número de pontos turisticos é: %d \n", turistico);
 
 
     //Informações usada como exemplo//
 
-    //Pais: Brasil//
-    //Estado: Pernambuco//
-    //Cidade: Caruaru//
-    //Carta: 2739//
-    //População: 378 mil//
-    //Área: 923 km²//
-    //PIB: 8 bilhões//
-    //Números de pontos turísticos: 7//
+    //Estado: Pernambuco
+    //Cidade: Caruaru
+    //Carta: A01
+    //População: 378 mil
+    //Área: 923.1 km²
+    //PIB: 8.1 bilhões
+    //Números de pontos turísticos: 7
 
     return 0;
 
