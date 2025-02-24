7f61a822d66c0e760f4fd8362a2c0795817b68cd
Wictor834 <wictorcesar16@gmail.com>
2025-02-24 18:07:24 +0000

    Finalizado nível novato

--- .vscode/Novato.c
+++ .vscode/Novato.c
@@ -2,7 +2,7 @@
 
 int main(){
     
-    char estado [15];
+    char estado;
     char cidade[20];
     char carta[10];
     int populacao;
@@ -11,7 +11,7 @@ int main(){
     int turistico;
 
     printf("Digite o estado: \n");
-    scanf("%s", &estado);
+    scanf("%c", &estado);
 
     printf("Digite a cidade: \n");
     scanf("%s", &cidade);
@@ -19,10 +19,10 @@ int main(){
     printf("Digite o número da carta: \n");
     scanf("%s", &carta);
 
-    printf("Digite o número da populacao: \n");
+    printf("Digite o número da população: \n");
     scanf("%d", &populacao);
 
-    printf("Digite a area: \n");
+    printf("Digite a área: \n");
     scanf("%f", &area);
 
     printf("Digite o PIB: \n");
@@ -31,24 +31,24 @@ int main(){
     printf("Digite o número de pontos turisticos: \n");
     scanf("%d" , &turistico);
 
-    printf("O nome do estado é: %s \n" , estado);
+    printf("O nome do estado é: %c \n" , estado);
     printf("O nome da cidade é: %s \n" , cidade);
     printf("O número da carta é: %s \n", carta);
-    printf("O número da população é: %d mil \n", populacao);
+    printf("O número da população é: %dM \n", populacao);
     printf("O tamanho da área em km² é: %f km² \n" , area);
     printf("O número do PIB é: %f bilhões \n", PIB);
     printf("O número de pontos turisticos é: %d \n", turistico);
 
 
-    //Informações usada como exemplo//
+                    //Informações usada como exemplo//
 
-    //Estado: Pernambuco
-    //Cidade: Caruaru
-    //Carta: A01
-    //População: 378 mil
-    //Área: 923.1 km²
-    //PIB: 8.1 bilhões
-    //Números de pontos turísticos: 7
+    //Estado: A //                            //Estado: B //
+    //Cidade: Salvador//                      //Cidade: Guarulhos//
+    //Carta: A01//                            //Carta: B01//
+    //População: 14 milhões//                 //População: 1 milhão//
+    //Área: 567.1 km²//                       //Área: 319.2 km²//
+    //PIB: 62 bilhões//                       //PIB: 77 bilhões//
+    //Números de pontos turísticos: 8//       //Números de pontos turísticos: 6//
 
     return 0;
 
